e0aa87c85882c083e5c51a13fa9c51bdb9dab377
dixonaws <dixonaws@amazon.co>
2018-04-23 09:59:51 -0400

    Added logic to read CSV into a list

--- createAccounts.py
+++ createAccounts.py
@@ -4,12 +4,15 @@ import csv
 
 
 def main():
-    accounts = {}
+    accounts = []
     with open('accountList.csv') as accountList:
         reader = csv.DictReader(accountList)
         for row in reader:
-            print row
+            accounts.append(row)
 
     accountList.close()
+
+    for account in accounts:
+        print account
 
 main()
