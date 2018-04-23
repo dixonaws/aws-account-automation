4f96d6ee953152f4603c0e3a3c0d38667b43de9a
dixonaws <dixonaws@amazon.com>
2018-04-23 10:11:22 -0400

    Added example commands to manipulate the account list

--- createAccounts.py
+++ createAccounts.py
@@ -5,6 +5,8 @@ import csv
 
 def main():
     accounts = []
+
+    # read the CSV file into a list called accounts
     with open('accountList.csv') as accountList:
         reader = csv.DictReader(accountList)
         for row in reader:
@@ -12,7 +14,15 @@ def main():
 
     accountList.close()
 
+    # print all the accounts
     for account in accounts:
         print account
+
+    numberOfAccounts = len(accounts)
+    print "Read " + str(numberOfAccounts) + " accounts from the input file"
+
+    # pop an account from the accounts list and print the email address of it
+    randomAccount = accounts.pop();
+    print "Email address from account 0: " + randomAccount['emailAddress']
 
 main()
