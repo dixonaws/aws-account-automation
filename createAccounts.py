93e5ff023c2ef8109c8d7ff1dc55a26333ff8959
dixonaws <dixonaws@amazon.com>
2018-04-24 13:34:55 -0400

    updated to trap KeyErrors

--- createAccounts.py
+++ createAccounts.py
@@ -9,8 +9,8 @@ def create_account(
         access_to_billing,
         organization_unit_id,
         scp):
-        print "create_account(): I got account " + account_email
-
+        print "create_account(): I got account " + account_name
+# create_account()
 
 def main():
     lstAccounts = []
@@ -26,9 +26,30 @@ def main():
     intNumberOfAccounts = len(lstAccounts)
     print "Read " + str(intNumberOfAccounts) + " accounts from the input file"
 
+    # print lstAccounts
+
+    account_name=""
+    account_email=""
+    account_role=""
+    access_to_billing=""
+    organization_unit_id=""
+    scp=""
 
     # print all the accounts
     for account in lstAccounts:
-        create_account("testaccount", account['emailAddress'],"","","","")
+        try:
+            account_name=account['account_name']
+            account_email=account['account_email']
+            account_role=account['account_role']
+            access_to_billing=account['access_to_billing']
+            organization_unit_id=account['organization_unit_id']
+
+        except KeyError:
+            pass
+
+        create_account(account_name, account_email, account_role,
+                       access_to_billing, organization_unit_id, scp)
+    # for account in lstAccounts
+# main()
 
 main()
